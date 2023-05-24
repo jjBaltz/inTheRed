609525087f6d61aa89154d3707f6d351362c4026
Jasper Baltz <nickolas.baltz@gmail.com>
2023-05-23 19:30:10 -0500

    added some user pages

---
+++ components/User.js
@@ -0,0 +1,24 @@
+import { PropTypes } from 'prop-types';
+import Card from 'react-bootstrap/Card';
+
+export default function User({ userObj }) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={userObj.photoURL} />
+      <Card.Body>
+        <Card.Title>{userObj.displayName}</Card.Title>
+        <Card.Text>
+          {userObj.email}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+User.propTypes = {
+  userObj: PropTypes.shape({
+    photoURL: PropTypes.string,
+    displayName: PropTypes.string,
+    email: PropTypes.string,
+  }).isRequired,
+};
