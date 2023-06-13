e45705bffe03bd0e354b3d5035c7823ddc9e9c3a
Jasper Baltz <nickolas.baltz@gmail.com>
2023-06-13 15:10:05 -0500

    worked on folders.js

--- pages/folders.js
+++ pages/folders.js
@@ -10,23 +10,27 @@ function Folders() {
           <Button id="folder-newEntry">New Entry</Button>
         </Link>
       </div>
-      <div className="folderIcons">
-        <Link href="/entries" passHref>
-          <Image
-            src="../assets/FolderIcon.png"
-            width={100}
-            height={100}
-          />
-        </Link>
-        <p className="subText">Submitted</p>
-        <Link href="/drafts" passHref>
-          <Image
-            src="../assets/FolderIcon.png"
-            width={100}
-            height={100}
-          />
-        </Link>
-        <p className="draftText">Drafts</p>
+      <div className="folders">
+        <div className="submitted">
+          <Link href="/entries" passHref>
+            <Image
+              src="../assets/FolderIcon.png"
+              width={100}
+              height={100}
+            />
+          </Link>
+          <p>Submitted</p>
+        </div>
+        <div className="drafts">
+          <Link href="/drafts" passHref>
+            <Image
+              src="../assets/FolderIcon.png"
+              width={100}
+              height={100}
+            />
+          </Link>
+          <p>Drafts</p>
+        </div>
       </div>
     </div>
   );
